c5636334649529a2e94f2afa384c86d09ddfa53e
ZCDEV <zoechangdev@gmail.com>
2020-03-25 23:37:19 -0700

    Revamp UI

--- public/assets/js/drinks.js
+++ public/assets/js/drinks.js
@@ -1,4 +1,11 @@
 $(function() {
+
+  var drinkClasses = ['drink1', 'drink2', 'drink3', 'drink4', 'drink5', 'drink6'];
+  $(".glass").each(function(event){
+      classIndex = Math.floor(Math.random() * drinkClasses.length);
+      $(this).addClass(drinkClasses[classIndex]);
+  })
+
   $(".drink-up").on("click", function(event) {
     var id = $(this).data("id");
     var drinkUp = $(this).data("newdrink");
@@ -7,14 +14,19 @@ $(function() {
       drink_up: drinkUp
     };
 
-    $.ajax("/api/drinks/" + id, {
-      type: "PUT",
-      data: newDrinkState
-    }).then(
-      function() {
-        location.reload();
-      }
-    );
+    $(this).prev().find(".glass").next().addClass("drinkup");
+        
+    setTimeout(function() {
+      $.ajax("/api/drinks/" + id, {
+        type: "PUT",
+        data: newDrinkState
+      }).then(
+        function() {
+          location.reload();
+        }
+      );
+    }, 5000);
+
   });
 
   $(".create-form").on("submit", function(event) {
